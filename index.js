70a91b5c9545a9652bd697e13c2e4025a41c1aa4
Zahra Said <zahra@zahrasaid.org>
2021-03-17 17:57:17 +0100

    Add index.js chalk

---
+++ index.js
@@ -0,0 +1,11 @@
+// const names =  require('./names');
+// console.log(greetings('bob')
+// )
+
+const chalk = require('chalk');
+console.log(chalk.blue('Hanna!'));
+console.log(chalk.green('Luca!'));
+console.log(chalk.red('Gege!'));
+console.log(chalk.yellow('Aliki!'));
+// Luca Milanese4:39 PM
+// touch .gitignore && echo "node_modules/" >> .gitignore && git rm -r --cached node_modules ; git status
